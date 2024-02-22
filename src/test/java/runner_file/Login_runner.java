cb111d95ac27fe68c257153f556ad9e080231440
Nethraraj123 <nethra.r@moolyaed.com>
2024-02-22 12:07:33 +0530

    commited changes

--- src/test/java/runner_file/Login_runner.java
+++ src/test/java/runner_file/Login_runner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)   // runwith is used when we have to run specific file
 
-@CucumberOptions(features= {"\\src\\test\\resources\\forms\\government_form.feature"},
+@CucumberOptions(features= {"C:\\QCCA22\\Arrays\\PART 3\\cucumber_project_junit\\src\\test\\resources\\forms\\government_form.feature"},
 glue={"steps"})
 
 public class Login_runner {
